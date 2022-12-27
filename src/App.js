6485a5a6b422e147e4744150c2bc616732fee7ac
shitalkhante <shitalkhante786@gmail.com>
2022-12-27 20:51:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import { useState, react, useEffect } from "react";
+import { useState } from "react";
 import './App.css';
 import { Card } from "./component/card";
 
 function App() {
   const [state, setState] = useState({ key: "", books: [] });
-  // const [books,setBooks] = useState([]);
 
-  useEffect(() => {
-    console.log(state.books)
-  }, [state]);
   async function getData() {
     await fetch(`https://www.googleapis.com/books/v1/volumes?q=${state.key}`).then(data => {
       return data.json();
     }).then(res => {
-      setState({ ...state, books: res });
-      console.log(state.books);
+      setState({ ...state, books: res.items });
     })
   }
 
@@ -29,16 +24,15 @@ function App() {
       </div>
       <div>
         {state.books.length > 0 &&
-          <Card />
-          // state.books.map((data, idx) => {
-          //   return (
-          //     <div><Card />
-          //       <h2>{data}</h2>
-          //       {/* <Card /> */}
-          //     </div>
-          //   )
-          // })
+        <div id="result">
+          {state.books.map((data, idx) => {
+            return (
+              <Card data={data.volumeInfo}/>
+            )
+          })}
+          </div>
         }
+        
       </div>
     </div>
   );
--- src/component/card.js
+++ src/component/card.js
@@ -1,9 +1,9 @@
 import "./card.css";
 import {useState} from "react"
-export const Card = () => {
+export const Card = (props) => {
     const [state,setState] = useState(false);
     return (
-        <div className="card" style={{ backgroundImage: 'url("http://books.google.com/books/content?id=s2rzYdI2BFcC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api")' }} onMouseOver={(e) => {
+        <div className="card" style={{ backgroundImage:props.data.imageLinks!==undefined?`url(${props.data.imageLinks.thumbnail})`:'url("http://books.google.com/books/content?id=CMPx-jksa6IC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")' }} onMouseOver={(e) => {
             e.preventDefault();
             e.currentTarget.style.transform = "scale(1.2)";
             setState(true);
@@ -11,12 +11,14 @@ export const Card = () => {
             // e.preventDefault();
             e.currentTarget.style.transform = "initial";
             setState(false);
+        }} onClick={()=>{
+            window.open(props.data.infoLink)
         }}>
             <div id="title" >
-                <h4 >harry potter</h4>
-                <h2 style={{display:state?'block':'none'}}>{"Felicity Baker"}</h2>
-                <p style={{display:state?'block':'none'}}>PAGE COUNT : {64}</p>
-                <p style={{display:state?'block':'none'}}>RATING : {4}</p>
+                <h4 >{props.data.title}</h4>
+                <h2 style={{display:state?'block':'none'}}>{props.data.authors[0]}</h2>
+                <p style={{display:state?'block':'none'}}>PAGE COUNT : {props.data.pageCount || 0}</p>
+                <p style={{display:state?'block':'none'}}>RATING : {props.data.ratingsCount}</p>
             </div>
 
         </div>
